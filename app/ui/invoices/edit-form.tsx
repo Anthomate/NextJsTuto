8175de8eab581105c9654ec5cd8656ed77719e7f
Anthomate <anthony.sapet@outlook.com>
2024-02-05 16:47:25 +0100

    Chapter 13-14

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -4,14 +4,12 @@ import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import { updateInvoice } from '@/app/lib/actions';
 
 export default function EditInvoiceForm({invoice, customers,}: {
-  invoice: InvoiceForm;
-  customers: CustomerField[];
+    invoice: InvoiceForm;
+    customers: CustomerField[];
 }) {
-  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+    const initialState = { message: null, errors: {} };
+    const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+    const [state, dispatch] = useFormState(updateInvoiceWithId, initialState);
 
-  return (
-      <form action={updateInvoiceWithId}>
-        <input type="hidden" name="id" value={invoice.id} />
-      </form>
-  );
+    return <form action={dispatch}></form>;
 }
